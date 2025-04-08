51ebf3c696845741acc9a69ce6c2d65872458384
OLEH FEDORCHUK <fedorchuk2018@gmail.com>
2025-04-08 23:53:16 +0100

    fixed bugs

--- bot.py
+++ bot.py
@@ -1,3 +1,46 @@
+from collections import UserDict
+from datetime import datetime
+import pickle
+
+def validate_phone(value):
+    """
+    Phone number validation:
+    - The number must consist of numbers only
+    - The number length must be from 9 to 14 digits
+    """
+    if not value.isdigit() or not (9 <= len(value) <= 14):
+        raise ValueError('The phone has to be 9 to 14 digits')
+    return value
+
+def validate_birthday(value):
+    '''
+    Birthday validation:
+    - Use the format DD.MM.YYYY
+    - Day, Month and Year must be correct
+    '''
+    parts = value.split('.')
+    if len(parts) != 3:
+        raise ValueError('Invalid date format. Use DD.MM.YYYY')
+    day_str, month_str, year_str = parts
+    if not (day_str.isdigit() and month_str.isdigit() and year_str.isdigit()):
+        raise ValueError('Invalid date format. Use DD.MM.YYYY')
+    day = int(day_str)
+    month = int(month_str)
+    year = int(year_str)
+    if not (1 <= month <= 12):
+        raise ValueError('Month must be between 1 and 12')
+    if len(year_str) != 4:
+        raise ValueError('Year must have 4 digits')
+    if month == 2:
+        max_day = 29 if (year % 4 == 0 and (
+            year % 100 != 0 or year % 400 == 0)) else 28
+    elif month in [4, 6, 9, 11]:
+        max_day = 30
+    else:
+        max_day = 31
+    if not (1 <= day <= max_day):
+        raise ValueError(f'Day must be between 1 and {max_day}')
+    return datetime(year, month, day).date()
 
 # -------------------------- Exception handler -----------------------------------------------
 def exception_handler(func):
@@ -35,6 +78,15 @@ class Phone(Field):
     def __init__(self, value):
         super().__init__(value)
 
+class Birthday(Field):
+    '''Class for sorting the Birthday'''
+
+    def __init__(self, value):
+        validated_date = validate_birthday(value)
+        super().__init__(validated_date)
+
+    def __str__(self):
+        return self.value.strftime('%d.%m.%Y')
 
 class Record:
     """Class for sorting the information about a contact, including name and phone list"""
@@ -75,7 +127,37 @@ class Record:
         return f'Contact name: {self.name}, phones: {phone_str} {bday_str}'
 
 # ------- add_contact, change_contact, show_phone, search_contacts, show_all, delete_contact ------------------------
-
+class AddressBook(UserDict):
+    """Класс для управления записями контактов."""
+    def add_record(self, record):
+        self.data[record.name.value] = record
+        
+    def find_record(self, name):
+        return self.data.get(name)
+    
+    def delete_record(self, name):
+        if name in self.data:
+            del self.data[name]
+            
+    def upcoming_birthday(self, days = 7):
+        '''
+        Returns a list of contacts with a birthday
+        '''
+        list_bday = []
+        today = datetime.now().date()
+        for record in self.data.values():
+            if record.birthday:
+                bday_this_year = record.birthday.value.replace(year = today.year)
+                if bday_this_year < today:
+                    bday_this_year = record.birthday.value.replace(year = today.year + 1)
+                if 0 <= (bday_this_year - today).days <= days:
+                    list_bday.append(record)
+                    
+        return list_bday
+def __str__(self):
+        if not self.data:
+            return 'List is empty'
+        return '\n'.join(str(record) for record in self.data.values())
 
 @exception_handler
 def add_contact(book, name, phone):
@@ -119,6 +201,51 @@ def delete_contact(book, name):
     raise KeyError  # 'Contact not found'
 
 
+@exception_handler
+def add_birthday_to_contact(book, name, birthday_str):
+    '''
+    Add or update Birthday
+    '''
+    record = book.find_record(name)
+    if not record:
+        record = Record(name)
+        book.add_record(record)
+    record.add_birthday(birthday_str)
+    return f'Birthday {birthday_str} added to contact {name}'
+
+def show_birthday (book, name):
+    record = book.find_record(name)
+    if record and record.birthday:
+        return f"{record.name.value}'s birthday is {record.birthday}"
+    return 'Birthday is not set for this contact'
+
+def upcoming_birthday(book):
+    '''
+    Returns list of contacts to send birthday wishes
+    '''
+    list_bday = book.get_upcoming_birthday()
+    if not list_bday:
+        return 'No upcoming birthday in the next week'
+    today = datetime.now().date()
+    lines = []
+    for record in list_bday:
+        bday_this_year = record.birthday.value.replace(year=today.year)
+        if bday_this_year < today:
+            bday_this_year = record.birthday.value.replace(year=today.year + 1)
+        days_left = (bday_this_year - today).days
+        lines.append(f'{record.name.value}:{record.birthday}(in {days_left} days)')
+    return '\n'.join(lines)
+
+def save_data (book, filename='addressbook.pkl'):
+    with open(filename, 'wb') as f:
+        pickle.dump(book, f)
+        
+def load_datа (filename='addressbook.pkl'):
+    try:
+        with open(filename, 'rb') as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
 def main():
     # book = AddressBook()
     book = load_datа()  # Download at the start
